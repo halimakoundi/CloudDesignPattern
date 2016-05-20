2f0b12a191941388fc6b3a2faeaa0ee7ac15ce18
Halima KOUNDI <hkoundi@gmail.com>
2016-05-20 13:08:15 +0100

    cache aside pattern
    trying to TDD the implementation

---
+++ CacheAsidePattern/CacheAsideTests/CacheAsideShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*
+ When data is not in store, the data is added to the store
+ When data is in store, the data is retrieved from the store
+ When data is modified , the data is removed from the store     
+ */
+
+namespace CacheAsideTests
+{
+    [TestClass]
+    public class CacheAsideShould
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
