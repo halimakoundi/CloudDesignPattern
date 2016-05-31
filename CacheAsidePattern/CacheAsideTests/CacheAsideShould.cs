8944c5440034f0bd0253b4374449cc16f3acf2cf
Halima KOUNDI <hkoundi@gmail.com>
2016-05-31 11:43:01 +0100

    renaming test

--- CacheAsidePattern/CacheAsideTests/CacheAsideShould.cs
+++ CacheAsidePattern/CacheAsideTests/CacheAsideShould.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CacheAsidePattern;
+﻿using CacheAsidePattern;
 using NUnit.Framework;
 /*
  When data is fetched and not in store, the data is added to the store, then the data is retrieved from the store
@@ -11,7 +10,7 @@ namespace CacheAsideTests
     public class CacheAsideShould
     {
         [Test]
-        public void add_user_to_store_if_user_is_not_in_store()
+        public void add_user_to_cache_if_user_is_not_in_cache()
         {
             var cacheAside = new DataAccessApi();
             var userId= "testUserId1";
