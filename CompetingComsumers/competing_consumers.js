66ef4e82bf154a45de2bd2e6b5739c90252a1d03
Halima KOUNDI <hkoundi@gmail.com>
2016-06-07 10:41:42 +0100

    requests get added to the queue as they are submitted

--- CompetingComsumers/competing_consumers.js
+++ CompetingComsumers/competing_consumers.js
@@ -1,19 +1,30 @@
-exports.application = function(){
-	return app();
+exports.application = function () {
+    return app();
 }
 
-var queue ;
+var queue;
 
-var app = function(){
-	this.start = startApplication;
-	this.getQueue = getQueue;	
-	
-	return this;
+var app = function () {
+    this.start = startApplication;
+    this.getQueue = getQueue;
+    this.submit = submit;
+    this.queueSize = queueSize;
+
+    return this;
 }
-var startApplication = function(){
-	if(!queue)
-		queue = [];
+var startApplication = function () {
+    if (!queue)
+        queue = [];
 }
-var getQueue = function(){
-	return queue;
+var getQueue = function () {
+    return queue;
+}
+var queueSize = function () {
+    return queue ? queue.length : 0;
+}
+
+var submit = function (request) {
+    if (request) {
+        queue.push(request);
+    }
 }
--- CompetingComsumers/spec/competing_consumers-spec.js
+++ CompetingComsumers/spec/competing_consumers-spec.js
@@ -4,16 +4,28 @@
 
 var competingComsumers = require("../competing_consumers.js");
 
-describe("Messages are added to the queue when new request is sent", function(){
-	
-	it("Should create queue when queue does not exist", function(){
-		var application = competingComsumers.application();
-		
-		application.start();
-		var queue = application.getQueue();
-		
-		expect(queue).toBeDefined()
-	});
-	
-	
+describe("Messages are added to the queue when new request is sent", function() {
+
+    it("Should create queue when queue does not exist",
+        function() {
+            var application = competingComsumers.application();
+
+            application.start();
+            var queue = application.getQueue();
+
+            expect(queue).toBeDefined();
+        });
+
+    it("should add messages to the queue when request is received",
+        function() {
+            var application = competingComsumers.application();
+            application.start();
+            var request = function () { console.log("Request execution"); };
+
+            application.submit(request);
+
+            var queueSize = application.queueSize();
+            expect(queueSize).toBe(1);
+        });
+
 });
